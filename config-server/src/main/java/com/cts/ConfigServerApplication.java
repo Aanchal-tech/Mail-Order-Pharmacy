e1f292952dd51ec8e1ad9a50e1ff5ddd7077e0c7
Aanchal-tech <aanchaldhawale@gmail.com>
2022-08-02 17:37:32 +0530

    Added log

--- config-server/src/main/java/com/cts/ConfigServerApplication.java
+++ config-server/src/main/java/com/cts/ConfigServerApplication.java
@@ -1,5 +1,7 @@
 package com.cts;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
@@ -7,8 +9,10 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
+	private static final Logger log = LoggerFactory.getLogger(ConfigServerApplication.class);
 
 	public static void main(String[] args) {
+		log.info("Config server running");
 		SpringApplication.run(ConfigServerApplication.class, args);
 	}
 
